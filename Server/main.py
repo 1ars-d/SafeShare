f8c7696722d69f22ebd6d8ab42de57b696bcd0e5
Coder-Lars <71517515+Coder-Lars@users.noreply.github.com>
2023-03-05 19:21:10 +0100

    stuff

--- Server/main.py
+++ Server/main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, session, request
-from flask_socketio import SocketIO
+from flask import Flask, render_template, session, request, redirect, url_for
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
+import datetime
 import sqlite3
+
+from utilities import generate_unique_code, room_exists
 
 # Server Setup
 app = Flask(__name__)
@@ -11,9 +14,17 @@ socketio = SocketIO(app)
 # Database Setup
 conn = sqlite3.connect("ROOMS_db.sqlite")
 cur = conn.cursor()
-cur.execute('CREATE TABLE rooms (code TEXT, members INTEGER)')
-cur.execute('CREATE TABLE messages (content TEXT, content_type TEXT, author TEXT, timestamp TEXT)')
-conn.commit
+# Check if tables exist
+cur.execute(
+    ''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='rooms' ''')
+if cur.fetchone()[0] != 1:
+    cur.execute('CREATE TABLE rooms (code TEXT, members INTEGER)')
+    cur.execute(
+        'CREATE TABLE messages (content TEXT, content_type TEXT, author TEXT, room TEXT, timestamp TEXT)')
+    cur.execute(
+        'CREATE TABLE logs (content TEXT, timestamp TEXT, room TEXT)')
+conn.close()
+
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -23,34 +34,103 @@ def home():
         code = request.form.get("code", "")
         join = request.form.get("join", False)
         create = request.form.get("create", False)
-
         # name empty
         if not name:
-            print("Hello")
-            print(code)
             return render_template("home.html", error="Please enter a name.", code=code, name=name)
         # code empty
         if join != False and not code:
             return render_template("home.html", error="Please enter a room code.", code=code, name=name)
 
         room = code
+
+        conn = sqlite3.connect("ROOMS_db.sqlite")
+        cur = conn.cursor()
         # create was pressed
         if create != False:
-            #create room
-            pass
-        
-        # -> case for: code doesnt exist
+            room = generate_unique_code(5)
+            cur.execute(
+                "INSERT INTO rooms(code, members) VALUES(?,?)", (room, 0))
+            conn.commit()
+            conn.close()
+            print("Created a new room with code:", room)
+        # Room doesn't exist
+        elif not room_exists(room):
+            return render_template("home.html", error="Room does not exist.", code=code, name=name)
 
         session["room"] = room
         session["name"] = name
-        
+        return redirect(url_for("room"))
+
         # -> send to room
     return render_template("home.html", name="", code="")
+
 
 @app.route("/room")
 def room():
     room = session.get("room")
-    return render_template("room.html", room="HDKT")
+    return render_template("room.html", room=room)
+
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    conn = sqlite3.connect("ROOMS_db.sqlite")
+    cur = conn.cursor()
+    if not room or not name:
+        return
+    if not room_exists(room):
+        leave_room(room)
+        return
+    join_room(room)
+    cur.execute(
+        f'UPDATE rooms SET members = members + 1 WHERE code = "{room}"')
+    conn.commit()
+    conn.close()
+    send_log(f"{name} joined the room.", room)
+    print(f"{name} joined room {room}")
+
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    conn = sqlite3.connect("ROOMS_db.sqlite")
+    cur = conn.cursor()
+
+    """ if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room] """
+
+    cur.execute(
+        f'UPDATE rooms SET members = members - 1 WHERE code = "{room}"')
+    conn.commit()
+    conn.close()
+    send_log(f"{name} left the room.", room)
+    print(f"{name} left the room {room}")
+
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if not room_exists(room):
+        return
+
+    content =
+
+
+def send_log(log, room):
+    conn = sqlite3.connect("ROOMS_db.sqlite")
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO logs(content, timestamp, room) VALUES(?,?,?)", (log, datetime.datetime.now().isoformat(), room))
+    conn.commit()
+    conn.close()
+    emit("log", {"log": log}, to=room)
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
--- Server/utilities.py
+++ Server/utilities.py
@@ -1,14 +1,22 @@
+import sqlite3
 from string import ascii_uppercase
 import random
 
-rooms = {}
 
 def generate_unique_code(length):
     while True:
         code = ""
         for _ in range(length):
             code += random.choice(ascii_uppercase)
-
-        if code not in rooms:
+        if not room_exists(code):
             break
     return code
+
+
+def room_exists(room):
+    conn = sqlite3.connect("ROOMS_db.sqlite")
+    cur = conn.cursor()
+    exists = cur.execute(
+        f'SELECT COUNT(1) FROM rooms WHERE code="{room}"').fetchone()[0] != 0
+    conn.close()
+    return exists
